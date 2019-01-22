f8e8e664cb515a54673810b30e4e331d3aebec14
mplp88 <martinp88@gmail.com>
2019-01-22 11:09:56 -0300

    Update

--- default.js
+++ default.js
@@ -1,6 +1,15 @@
 function loginFailed(salida){
   try {
-    alert("Error ejecutando el Login. \n" + salida);
+
+    swal("Error", "Error ingresando a la página.", "error")
+    .then(() => {
+      var errorCont =document.getElementById('error-messages')
+      var oSp = document.createElement('span');
+      oSp.innerHTML = salida;
+      errorCont.classList.add('alert');
+      errorCont.classList.add('alert-danger');
+      errorCont.appendChild(oSp);
+    });
     return true;
   } catch (error) {
     return false;
